776b53d38bdddafab4e268b381a421cb664d5866
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-22 12:16:16 -0500

    Completed version of Bank Encapsulation Project

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        // Default Constructor
+        public BankAccount()
+        {
+        }
+
+
+        // Private Field
+        private double _balance = 0;
+
+
+        // Public Method Deposit
+        public double Deposit(double addBalance)
+        {
+            return (_balance += addBalance);
+        }
+
+        // Public Method GetBalance
+        public void GetBalance()
+        {
+            Console.WriteLine($"You have successfully deposited ${ _balance } into your bank account.");
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,7 +6,46 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount userAccount = new BankAccount();
+
+            // Enter Name
+            Console.WriteLine("Please enter your first and last name: ");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("");
+
+            while (firstName == "" || lastName == "")
+            {
+                Console.WriteLine("Please enter a valid name.");
+                Console.Write("First Name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                lastName = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+
+            // Deposit Amount
+            Console.WriteLine($"Welcome, {firstName} {lastName}.");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter the amount (in $USD) you are depositing into your account.");
+
+            double userDeposit = double.Parse(Console.ReadLine());
+
+            while (userDeposit < 0)
+            {
+                Console.WriteLine("Please enter a valid number (cannot be negative or 0).");
+                userDeposit = double.Parse(Console.ReadLine());
+            };
+
+            // Deposit Confirmation
+            userAccount.Deposit(userDeposit);
+            userAccount.GetBalance();
+
+            Console.WriteLine("Thank you. Have a nice day.");
+
         }
     }
 }
